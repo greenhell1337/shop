5e931e350c5454e29bd22eb5c71f7301cafcb3da
Ed <edik190703@gmail.com>
2023-02-12 00:16:18 +0500

    Commit

--- pkg/othercode.go
+++ pkg/othercode.go
@@ -1,10 +1,5 @@
 package pkg
 
-import (
-	"io/ioutil"
-	"os"
-)
-
 func SumCost(card map[string]int) int {
 	var sum int
 	for _, v := range card {
@@ -20,22 +15,4 @@ func Discount(amount int) int {
 		amount = amount - (amount / 10)
 	}
 	return amount
-}
-
-func CreateFile(name string) (bool, error) {
-	_, err := os.Create("./pkg/" + name)
-	if err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
-func OpenFile(name string, Order any) (bool, error){
-	file, err := os.Open("./pkg/" + name)
-	if err != nil {
-		return false, err
-	}
-	for {
-		os.E
-	}
 }
